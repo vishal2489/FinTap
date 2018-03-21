573c98cb08c9f76e171f6d7a0a0f126c94a5ebc3
My lappy <My lappy@ADMIN>
2018-03-21 22:11:21 +0530

    Add project files.

---
+++ FinTap/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinTap.MvcApplication" Language="C#" %>
---
+++ FinTap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinTap.Startup))]
+namespace FinTap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
